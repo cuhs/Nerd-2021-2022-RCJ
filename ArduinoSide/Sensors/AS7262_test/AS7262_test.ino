7d1aa4f4846d5e5539356937252093f220c48ce1
Alex <alexhu00374@gmail.com>
2022-06-04 16:32:42 -0400

    update test to detect colors

--- ArduinoSide/Sensors/AS7262_test/AS7262_test.ino
+++ ArduinoSide/Sensors/AS7262_test/AS7262_test.ino
@@ -32,14 +32,15 @@ void setup() {
   while(!Serial);
   
   // initialize digital pin LED_BUILTIN as an output.
-  //pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(7, OUTPUT);
-  digitalWrite(7,HIGH);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(8, OUTPUT);
+  digitalWrite(8,HIGH);
   //begin and make sure we can talk to the sensor
   if(!ams.begin()){
     Serial.println("could not connect to sensor! Please check your wiring.");
     while(1);
   }
+    ams.drvOn();
 }
 
 void loop() {
@@ -48,7 +49,7 @@ void loop() {
   uint8_t temp = ams.readTemperature();
  
   
-  //ams.drvOn(); //uncomment this if you want to use the driver LED for readings
+   //uncomment this if you want to use the driver LED for readings
   ams.startMeasurement(); //begin a measurement
   
   //wait till data is available
@@ -63,27 +64,51 @@ void loop() {
   //ams.drvOff(); //uncomment this if you want to use the driver LED for readings
 
   //read the values!
- // time = millis();
   ams.readRawValues(sensorValues);
-//  time = millis() - time;
-//  Serial.print(" Time for readRawValues: "); Serial.print(time);
-  //ams.readCalibratedValues(calibratedValues);
 
   //Serial.print(" Temp: "); Serial.print(temp);
+//  uint8_t v=sensorValues[AS726x_VIOLET];
+//  uint8_t b=sensorValues[AS726x_BLUE];
+//  uint8_t g=sensorValues[AS726x_GREEN];
+//  uint8_t y=sensorValues[AS726x_YELLOW];
+//  uint8_t o=sensorValues[AS726x_ORANGE];
+//  uint8_t r=sensorValues[AS726x_RED];
+//  
+//  Serial.print(" Violet:"); Serial.print(v); Serial.print(" ");
+//  Serial.print(" Blue:"); Serial.print(b); Serial.print(" ");
+//  Serial.print(" Green:"); Serial.print(g); Serial.print(" ");
+//  Serial.print(" Yellow:"); Serial.print(y);Serial.print(" ");
+//  Serial.print(" Orange:"); Serial.print(o); Serial.print(" ");
+//  Serial.print(" Red:"); Serial.print(r); Serial.print(" ");
+//  Serial.print(" Avg:"); Serial.println((v+b+g+y+o+r)/5);
+detectTiles();
+}
+
+void detectTiles(){
+  ams.startMeasurement();
+  bool rdy=false;
+  while(!rdy){
+    delay(5);
+    rdy = ams.dataReady();
+  }
+  ams.readRawValues(sensorValues);
   uint8_t v=sensorValues[AS726x_VIOLET];
   uint8_t b=sensorValues[AS726x_BLUE];
   uint8_t g=sensorValues[AS726x_GREEN];
   uint8_t y=sensorValues[AS726x_YELLOW];
   uint8_t o=sensorValues[AS726x_ORANGE];
   uint8_t r=sensorValues[AS726x_RED];
-  
-  Serial.print(" Violet: "); Serial.print(v);
-  Serial.print(" Blue: "); Serial.print(b);
-  Serial.print(" Green: "); Serial.print(g);
-  Serial.print(" Yellow: "); Serial.print(y);
-  Serial.print(" Orange: "); Serial.print(o);
-  Serial.print(" Red: "); Serial.print(r);
-  Serial.print(" Avg: "); Serial.print((v+b+g+y+o+r)/5);
-  Serial.println();
-  Serial.println();
+  int avg = (v+b+g+y+o+r)/5;
+  int range = maxSix(v,b,g,y,o,r)-minSix(v,b,g,y,o,r);
+  if(range<=40 && avg <=15)
+    Serial.println("Black detected");
+  else if(range >= 150)
+    Serial.println("Silver detected");
+}
+
+int maxSix(int a, int b, int c, int d, int e, int f){
+  return max(max(max(a, b),max(c,d)),max(e,f));
+}
+int minSix(int a, int b, int c, int d, int e, int f){
+  return min(min(min(a, b),min(c,d)),min(e,f));
 }
