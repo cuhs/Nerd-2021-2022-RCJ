9a327f4e6844abd3819ec4a46a1d722015a01bde
Alex <alexhu00374@gmail.com>
2022-06-07 19:48:44 -0400

    adjust color test

--- ArduinoSide/Sensors/AS7262_test/AS7262_test.ino
+++ ArduinoSide/Sensors/AS7262_test/AS7262_test.ino
@@ -97,6 +97,7 @@ void loop() {
   Serial3.print(" Red:"); Serial3.print(r); Serial3.print(" ");
   Serial3.print(" Avg:"); Serial3.println((v+b+g+y+o+r)/5);
   }
+//printRange();
 //detectTiles();
 }
 
@@ -127,4 +128,23 @@ int maxSix(int a, int b, int c, int d, int e, int f){
 }
 int minSix(int a, int b, int c, int d, int e, int f){
   return min(min(min(a, b),min(c,d)),min(e,f));
+}
+
+void printRange(){
+  ams.startMeasurement();
+  bool rdy=false;
+  while(!rdy){
+    delay(5);
+    rdy = ams.dataReady();
+  }
+  ams.readRawValues(sensorValues);
+  uint8_t v=sensorValues[AS726x_VIOLET];
+  uint8_t b=sensorValues[AS726x_BLUE];
+  uint8_t g=sensorValues[AS726x_GREEN];
+  uint8_t y=sensorValues[AS726x_YELLOW];
+  uint8_t o=sensorValues[AS726x_ORANGE];
+  uint8_t r=sensorValues[AS726x_RED];
+  int avg = (v+b+g+y+o+r)/5;
+  int range = maxSix(v,b,g,y,o,r)-minSix(v,b,g,y,o,r);
+  Serial3.print(" range: "); Serial3.println(range);
 }
